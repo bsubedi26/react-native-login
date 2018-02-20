2a9fd5b652dcc080821d052e31558b59cd24324c
bsubedi26 <bsubedi26@gmail.com>
2018-02-20 14:54:38 -0500

    feathers/redux initial config

---
+++ src/util/feathers.js
@@ -0,0 +1,25 @@
+import { AsyncStorage } from 'react-native';
+import feathers from '@feathersjs/client/dist/feathers.min';
+import auth from '@feathersjs/client/dist/authentication.min';
+import socketio from '@feathersjs/client/dist/socketio.min';
+import io from 'socket.io-client';
+// import fRest from '@feathersjs/rest-client';
+
+const HOST = 'http://192.168.0.18:3030';
+const socketOptions = { transports: ['websocket'] };
+const socket = io(HOST, socketOptions);
+// const restClient = fRest(HOST)
+
+const app = feathers()
+  .configure(socketio(socket))
+  // .configure(restClient.fetch(fetch))
+  .configure(auth({
+      storage: AsyncStorage
+    }));
+
+app.io.on('connect_error', error => {
+  console.log('Could not connect to server. Closing socket connection.');
+  app.io.close();
+})
+
+export default app
