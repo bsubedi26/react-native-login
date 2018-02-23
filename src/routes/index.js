16c3eabd155edb4bda75fcae46d63f09b687e01c
bsubedi26 <bsubedi26@gmail.com>
2018-02-22 19:03:40 -0500

    react navigation router components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,71 +1,54 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { View, Image, Text, TouchableOpacity, Alert } from "react-native";
 import styles from "./styles";
 import { Router, Stack, Scene } from "react-native-router-flux";
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import SideMenu from 'src/components/SideMenu';
 import AuthActions from "src/store/auth/action";
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
 
 /**
  * ROOT ROUTES PAGES
  */
 import { Login, Signup, Home } from "../pages";
 
-const options = {
+
+const menuPressed = (navigation) => navigation.navigate('DrawerOpen')
+const drawerIcon = (navigation) => <Ionicons style={styles.leftIcon} onPress={menuPressed.bind(this, navigation)} name="md-menu" size={32} color="black" />
+
+const drawerOptions = {
   contentComponent: SideMenu,
-  drawerWidth: 256
+  drawerWidth: 256,
+  drawerPosition: 'left',
+  initialRouteName: 'Home',
 };
 
 
+export const DrawerNav = DrawerNavigator({
+  Login: { screen: Login },
+  Signup: { screen: Signup },
+  Home: { screen: Home },
+}, drawerOptions)
+
+export const Navigator = StackNavigator({
+  Main: {
+    screen: DrawerNav,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerLeft: drawerIcon(navigation)
+      }
+    }
+  },
+}, {
+    initialRouteName: 'Main',
+})
+
 class RootRoutes extends React.Component {
-  drawerIcon = () => <Ionicons style={styles.leftIcon} name="md-menu" size={32} color="black" />
-  
-  avatarPressed = () => {
-    Alert.alert('Confirm Logout', 'Are you sure you want to log out?',
-      [
-        {
-          text: 'Cancel', onPress: () => {
-          }, style: 'cancel'
-        },
-        { text: 'Yes', onPress: this.logout, style: 'destructive' },
-      ]
-    );
-  }
-
-  logout = async () => {
-    const { dispatch } = this.props
-    dispatch(AuthActions.logout())
-  }
-
-  renderNavRightIcon = (avatar) => {
-    return (
-      <TouchableOpacity style={{ padding: 5 }} onPress={this.avatarPressed}>
-        <Image
-          source={{ uri: `data:image/png;base64,${avatar}` }}
-          style={{ height: 40, width: 40, borderRadius: 40 }}
-        />
-      </TouchableOpacity>
-    )
-  }
-
   render() {
-    const { auth } = this.props
-    
     return (
-      <Router>
-        {/* <Scene drawer={true} {...options} drawerIcon={drawerIcon()} onRight={() => console.log("Right icon pressed.")} rightButtonImage={auth.avatar ? rightIcon(auth.avatar) : <Text>Login</Text>} key="root"> */}
-        <Scene drawer={true} {...options} drawerIcon={this.drawerIcon()} renderRightButton={auth.avatar ? this.renderNavRightIcon(auth.avatar) : <Text></Text>} key="root">
-          <Scene initial key="login" component={Login} title="Login" />
-          <Scene key="signup" component={Signup} title="Signup" />
-          <Scene key="home" component={Home} title="Home" />
-        </Scene>
-      </Router>
+      <Navigator />
     )
   }
 }
 
-const mapState = state => ({
-  auth: state.auth
-})
-export default connect(mapState)(RootRoutes);
+export default RootRoutes
