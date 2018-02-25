4437c1a1a69eeba98beaaf98cbccbb90b870e22d
bsubedi26 <bsubedi26@gmail.com>
2018-02-24 21:59:57 -0500

    localhost url platform select

--- src/util/feathers.js
+++ src/util/feathers.js
@@ -1,25 +1,26 @@
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, Platform } from 'react-native';
 import feathers from '@feathersjs/client/dist/feathers.min';
 import auth from '@feathersjs/client/dist/authentication.min';
 import socketio from '@feathersjs/client/dist/socketio.min';
 import io from 'socket.io-client';
-// import fRest from '@feathersjs/rest-client';
 
-const HOST = 'http://192.168.0.18:3030';
+const HOST = Platform.select({
+  ios: "http://localhost:3030",
+  android: "http://192.168.0.18:3030"
+});
+
 const socketOptions = { transports: ['websocket'], forceNew: true };
 const socket = io(HOST, socketOptions);
-// const restClient = fRest(HOST)
 
 const app = feathers()
   .configure(socketio(socket))
-  // .configure(restClient.fetch(fetch))
   .configure(auth({
       storage: AsyncStorage
-    }));
+  }));
 
 app.io.on('connect_error', error => {
   console.log('Could not connect to server. Closing socket connection.');
   app.io.close();
 })
 
-export default app
+export default app;
